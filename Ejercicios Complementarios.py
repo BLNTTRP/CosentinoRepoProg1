16ccaaef79a60b81edfcb592320d5ecdc9ee533b
Joaquin Cosentino <joacocosentino2003@outlook.com.ar>
2023-10-05 01:05:11 -0300

    Ejercicios complementarios Clase 28/9 Cosentino Joaquin

---
+++ Ejercicios Complementarios.py
@@ -0,0 +1,53 @@
+numero1 = 12
+print("Numero 1: ", numero1)
+numero2 = 3.5
+print("Numero 2: ", numero2)
+suma = numero1 + numero2
+print("Suma: ", suma)
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+if numero1 >= numero2:
+    division = numero1 / numero2
+else:
+    division = numero2 / numero1
+print("Resta: ", resta)
+print("Multiplicacion: ", multiplicacion)
+print("Division: ", division)
+nombre = "Joaquin"
+print("Mi nombre: ", nombre)
+precio = 240.38
+print("Precio del articulo: $", precio)
+descuento = 0.25
+print("Porcentaje de descuento (en decimal): ", descuento)
+precio_final = precio - (precio * descuento)
+print("Precio final del producto (con descuento aplicado): ", precio_final)
+cadena = "Hola mundo, soy Joaquin!"
+print("Cadena creada: ", cadena)
+longitud = len(cadena)
+print("Longitud de la cadena: ", longitud)
+precio_decimal = 23.5
+precio_entero = int(precio_decimal)
+print("Precio en decimal: ", precio_decimal)
+print("Precio decimal convertido a entero: ", precio_entero)
+nombre = "Joaquin"
+apellido = "Cosentino"
+nombre_completo = nombre + " " + apellido
+print("Nombre completo: ", nombre_completo)
+edad = 20
+print("Mi edad: ", edad)
+edad += 5
+print("Mi edad + 5: ", edad)
+edad -= 10
+print("Mi edad + 5 - 10: ", edad)
+altura = 1.72
+print("Mi altura: ", altura)
+altura *= 4
+print("Mi altura * 4: ", altura)
+altura /= 3
+print("Mi altura * 4 / 3: ", altura)
+nombre_MAYUS = "JOAQUIN"
+print("Mi nombre en mayus: ", nombre_MAYUS)
+nombre_minus = nombre_MAYUS.lower()
+print("Mi nombre en minus: ", nombre_minus)
+nombre_Title = nombre_MAYUS.title()
+print("Mi nombre bien escrito: ", nombre_Title)
